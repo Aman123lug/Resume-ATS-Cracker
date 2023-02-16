6f6c3d9a7b328262506d304c522b663c158aeb9d
Aman123lug <ak06465676@gmail.com>
2023-02-16 15:36:19 +0530

    first commit

---
+++ First.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import base64,PyPDF2
+
+st.title('Automatic Resume Quality Checker')
+
+file = st.file_uploader('Upload your resume here (.pdf only)',['.pdf'],accept_multiple_files=False)
+
+def pdf_viewer(file_path):
+    with open(save_pdf,'rb') as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.sidebar.markdown(pdf_display, unsafe_allow_html=True)
+    
+
+def data_extracttor(fp):
+    global score
+
+    pdf = PyPDF2.PdfReader(fp)
+
+    st.write('_____________________________________________________________________________')
+    # taking out the title or name from pdf
+    data = pdf.metadata
+    st.header(f'Hi, {data.title}')
+    
+    # counting the number of pages and scoring based on that 
+    numofpg = len(pdf.pages)
+    if numofpg == 1:
+        score += 1
+        st.subheader("Looks like, you're Fresher. But your length of resume is good ")
+    elif numofpg <= 3:
+        score += 1
+        st.subheader("Looks like, you got some Experience ")
+    else:
+        st.subheader("Your Resume is Quite Lengthy ")
+        score -= 1
+
+    text = ''
+    for i in range(numofpg):
+         
+        text += pdf.pages[i].extract_text()
+    
+    return text 
+
+if file:
+    
+    save_pdf = './uploaded resumes/'+ file.name 
+
+    with open(save_pdf,'wb') as sa:
+        sa.write(file.getbuffer())
+
+    pdf_viewer(save_pdf)
+    
+    score = 0
+    text = data_extracttor(save_pdf)
+    st.write(text)
+    
+    print(text)
+    text = text.lower()
+    
+    
+    
+    
+    
