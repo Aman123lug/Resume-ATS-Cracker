9704f67374b67bc79d7e6b36438f08e9ce0f7243
Aman123lug <ak06465676@gmail.com>
2023-02-18 23:01:41 +0530

    second commit by aman

--- First.py
+++ First.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import base64,PyPDF2
-from pyresparser import ResumeParser
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 
 
-st.title('Automatic Resume Quality Checker')
+st.title('MNC Resume ATS Cracker 📃')
 
 file = st.file_uploader('Upload your resume here (.pdf only)',['.pdf'],accept_multiple_files=False)
 
@@ -15,6 +17,19 @@ def pdf_viewer(file_path):
     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     st.sidebar.markdown(pdf_display, unsafe_allow_html=True)
     
+
+stop_words = set(stopwords.words("english")+['``',"''"])
+
+def cleanText(resumeText):
+    resumeText = re.sub("http/S+/s*", '', resumeText)
+    resumeText = re.sub('RT|cc', ' ', resumeText)  # remove RT and cc
+    resumeText = re.sub('#\S+', '', resumeText)  # remove hashtags
+    resumeText = re.sub('@\S+', '  ', resumeText)  # remove mentions
+    resumeText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', resumeText)  # remove punctuations
+    resumeText = re.sub(r'[^\x00-\x7f]',r' ', resumeText) 
+    resumeText = re.sub('\s+', ' ', resumeText)
+    resumeText = re.sub(r'[0-9]', "", resumeText)
+    return resumeText
 
 def data_extracttor(fp):
     global score
@@ -45,6 +60,8 @@ def data_extracttor(fp):
     
     return text 
 
+
+
 if file:
     
     save_pdf = './uploaded resumes/'+ file.name 
@@ -53,13 +70,38 @@ if file:
         sa.write(file.getbuffer())
 
     pdf_viewer(save_pdf)
-    resume_data = ResumeParser(save_pdf).get_extracted_data()
     
     score = 0
-    # text = data_extracttor(save_pdf)
-    st.write(resume_data)
+    text = data_extracttor(save_pdf)
+    
+    # Data Cleaning Part start from here ! 
+    data = []
+    clean_data = ""
+    for i in text:
+        temp_data = cleanText(i)
+        clean_data += temp_data
+        words = word_tokenize(clean_data)
+        print(words)
+        
+    for j in words:
+        if j not in stop_words:
+            data.append(j)
+
+    full_data = " ".join(data)
+    st.write(full_data)
+        
+        # if words not in stop_words:
+        #     data.append(words)
+            
+    # full_data = " ".join(words)
+    # st.write(full_data)
+            
+            
     
     
+    
+
+   
     
     
     
